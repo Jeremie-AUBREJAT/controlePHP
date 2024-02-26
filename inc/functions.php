8d24a2cb8bf8165886f8fda8741c42e440757954
JeremieABJT <jeremy.aubrejat@gmail.com>
2024-02-26 15:35:39 +0100

    15:35

--- inc/functions.php
+++ inc/functions.php
@@ -44,28 +44,58 @@ if(isset($_POST['submit']))
         echo "erreur d envoi";
     }
 };
-    session_start();
-    if(isset($_POST['submit']))
-    {
-        // On vérifie si les champs ont une valeur
-        if(!empty($_POST['username']) && !empty($_POST['password']) && !empty($_POST['confirm-password']))
-        {
-            $motdepasse = password_hash($_POST['password'],PASSWORD_DEFAULT);
-            $_SESSION['username'] = $_POST['username'];
-            $_SESSION['password'] = $motdepasse;
-    
-            {
-                // On prépare notre ligne CSV
-                $donnees = $_POST['username'].';'.$motdepasse."\n";
-                // J'insère ma nouvelle ligne
-                fwrite($fp,$donnees);
-                // Je ferme mon fichier
-                fclose($fp);
-                echo 'Inscription réussie';
-    
-            }
-        }
-    
-    };
+// require_once('db.php');
+// Je vérifie si le formulaire a été envoyé
+// if(isset($_POST['submit']))
+// {
+//     // Je vérifie si mes champs ont une valeur
+//     if(!empty($_POST['email']) && !empty($_POST['password']))
+//     {
+//         // Je fais ma requête pour vérifier si j'ai un user
+//         $req = $dbh->query('SELECT * FROM users WHERE email = "'.$_POST['email'].'" LIMIT 1');
+//         if($req->rowCount() == 1)
+//         {
+//             // Je range les infos du user dans un tableau associatif
+//             $user = $req->fetch(PDO::FETCH_ASSOC);
+//             // Je vérifie si le mot de passe est bon
+//             if(password_verify($_POST['password'],$user['password'])){
+//                 // On va lui envoyer un mail
+//                 use PHPMailer\PHPMailer\PHPMailer;
+//                 use PHPMailer\PHPMailer\SMTP;
+//                 use PHPMailer\PHPMailer\Exception;
 
+//                 //Load Composer's autoloader
+//                 require 'vendor/autoload.php';
+
+//                 //Create an instance; passing `true` enables exceptions
+//                 $mail = new PHPMailer(true);
+
+//                 try {
+                    
+//                     $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      
+//                     $mail->isSMTP();                                          
+//                     $mail->Host       = 'dwwm2324.fr';                     
+//                     $mail->Username   = 'contact@dwwm2324.fr';                     
+//                     $mail->Password   = 'm%]E5p2%o]yc';                             
+//                     $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;           
+//                     $mail->Port       = 465;                                    
+
+                    
+//                     $mail->setFrom('contact@dwwm2324.fr', 'PHP life');
+//                     $mail->addAddress($user['email'],$user['nom']);     
+                  
+//                     $mail->isHTML(true);                                  
+//                     $mail->Subject = 'Connexion à votre compte';
+//                     $mail->Body    = 'Un utilisateur vient de se connecter (ip ='.$_SERVER['REMOTE_ADDR'].')';
+//                     $mail->AltBody = 'Un utilisateur vient de se connecter (ip ='.$_SERVER['REMOTE_ADDR'].')';
+
+//                     $mail->send();
+//                     echo 'Vous êtes connecté';
+//                 } catch (Exception $e) {
+//                     echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+//                 }
+//             }
+//         }
+//     }
+// }
 ?>
